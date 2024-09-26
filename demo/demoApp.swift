285e231a8a65f1e41583d54a4a3571e018db390c
Wajan <wajanaltalhan@gmail.com>
2024-09-26 12:40:49 +0300

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Wajan Altalhan on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demoTests/demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  demoTests.swift
+//  demoTests
+//
+//  Created by Wajan Altalhan on 22/03/1446 AH.
+//
+
+import Testing
+@testable import demo
+
+struct demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
